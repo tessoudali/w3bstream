eb3405fe5ff1ca58d51303c2f197b2b593d36188
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2022-09-14 16:44:07 +0800

    define event (#3)

--- pkg/modules/event/event.go
+++ pkg/modules/event/event.go
@@ -1,3 +1,14 @@
 // event management
 
 package event
+
+type Event interface {
+	Meta() MetaData
+	Raw() []byte
+}
+
+type MetaData struct {
+	PublisherID string
+	ProjectID   string
+	AppletID    string // optional
+}
