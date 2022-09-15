34866fefff257d7d7dbb71e02f2a08107f059e63
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2022-09-15 17:25:44 +0800

    event dispatch

--- pkg/modules/event/dispatcher.go
+++ pkg/modules/event/dispatcher.go
@@ -1,7 +1,22 @@
 package event
 
-type dispatcher struct{}
+type dispatcher struct {
+	apiServerURI string
+	vmManagerURI string
+}
 
 func (d *dispatcher) dispatch(e Event) error {
+	id, err := d.vmInstance(e.Meta())
+	if err != nil {
+		return err
+	}
+	return d.vmCall(id, e.Raw())
+}
+
+func (d *dispatcher) vmInstance(e MetaData) (string, error) {
+	return "", nil
+}
+
+func (d *dispatcher) vmCall(vmID string, data []byte) error {
 	return nil
 }
