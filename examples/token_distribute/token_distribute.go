90e7a1fb6d8ad74c3ae4b8c34f4d4fe22fb17608
zhi <thecoderzhi@gmail.com>
2022-10-17 00:56:55 -0700

    update

--- examples/token_distribute/token_distribute.go
+++ examples/token_distribute/token_distribute.go
@@ -53,9 +53,22 @@ func _start(rid uint32) int32 {
 	log(fmt.Sprintf("wasm >> receive message: %s", str))
 	count := getValue("clicks") + 1
 	if count%5 == 0 {
+		/*
+			ptr, size := stringToPtr(fmt.Sprintf(
+				`{
+					"to": "%s",
+					"value": "0",
+					"data": "40c10f19000000000000000000000000%s0000000000000000000000000000000000000000000000000de0b6b3a7640000"
+				}`,
+				"0x1ED83F5AD999262eC06Ed8f3B801e108024b3e9c",
+				"97186a21fa8e7955c0f154f960d588c3aca44f14",
+			))
+			_ws_send_tx(ptr, size)
+		*/
+		log("wasm << send tx")
 		// call _ws_send_tx
 	}
-	setValue("clics", count)
+	setValue("clicks", count)
 	return 0
 }
 
