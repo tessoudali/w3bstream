7090bd812c90a06b2e16bfd65e8c3d42fbef8cd7
saito <birdyfj@gmail.com>
2023-05-19 15:53:46 +0800

    test(httptransport/client): add unit test (#589)
    * test(httptransport/client): add unit test
    
    * chore: update go.mod
    
    ---------
    
    Co-authored-by: CoderZhi <thecoderzhi@gmail.com>

--- go.mod
+++ go.mod
@@ -46,6 +46,7 @@ require (
 	github.com/minio/minio-go/v7 v7.0.52
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spruceid/siwe-go v0.2.0
+	github.com/stretchr/testify v1.8.2
 )
 
 require (
@@ -53,6 +54,7 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd v0.20.1-beta // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dchest/uniuri v0.0.0-20200228104902-7aecb25e1fe5 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -76,6 +78,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/openzipkin/zipkin-go v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
---
+++ pkg/depends/kit/httptransport/client/client_test.go
@@ -0,0 +1,126 @@
+package client_test
+
+import (
+	"context"
+	"encoding/xml"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/machinefi/w3bstream/pkg/depends/kit/httptransport/client"
+	"github.com/machinefi/w3bstream/pkg/depends/kit/httptransport/httpx"
+	"github.com/machinefi/w3bstream/pkg/depends/kit/httptransport/mock"
+	"github.com/machinefi/w3bstream/pkg/depends/kit/statusx"
+)
+
+type IpInfo struct {
+	xml.Name    `xml:"query"`
+	Country     string `json:"country" xml:"country"`
+	CountryCode string `json:"countryCode" xml:"countryCode"`
+}
+
+type GetByJSON struct {
+	httpx.MethodGet
+}
+
+func (GetByJSON) Path() string {
+	return "/me.json"
+}
+
+type GetByXML struct {
+	httpx.MethodGet
+}
+
+func (GetByXML) Path() string {
+	return "/me.xml"
+}
+
+func TestClient(t *testing.T) {
+	cli := &client.Client{
+		Protocol: "https",
+		Host:     "ip.nf",
+		Timeout:  100 * time.Second,
+	}
+	cli.SetDefault()
+
+	t.Run("direct request", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "https://api.github.com", nil)
+		_, err := cli.Do(context.Background(), req).Into(nil)
+		require.NoError(t, err)
+	})
+
+	t.Run("direct request 404", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "https://api.github.com/xxxxn", nil)
+
+		meta, err := cli.Do(context.Background(), req).Into(nil)
+		require.Error(t, err)
+
+		t.Log(err)
+		t.Log(meta)
+	})
+
+	t.Run("request by struct", func(t *testing.T) {
+		rsp := IpInfo{}
+
+		meta, err := cli.Do(context.Background(), &GetByJSON{}).Into(&rsp)
+		require.NoError(t, err)
+
+		t.Log(rsp)
+		t.Log(meta)
+	})
+
+	t.Run("request by struct as xml", func(t *testing.T) {
+		rsp := IpInfo{}
+
+		meta, err := cli.Do(context.Background(), &GetByXML{}).Into(&rsp)
+		require.NoError(t, err)
+
+		t.Log(rsp)
+		t.Log(meta)
+	})
+
+	t.Run("cancel request", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(1 * time.Millisecond)
+			cancel()
+		}()
+
+		rsp := IpInfo{}
+		_, err := cli.Do(ctx, &GetByJSON{}).Into(&rsp)
+		require.Equal(t, "ClientClosedRequest", err.(*statusx.StatusErr).Key)
+	})
+
+	t.Run("err request", func(t *testing.T) {
+		errcli := &client.Client{
+			Timeout: 100 * time.Second,
+		}
+		errcli.SetDefault()
+
+		{
+			rsp := IpInfo{}
+
+			_, err := errcli.Do(
+				client.ContextWithClient(
+					context.Background(),
+					client.GetShortConnClientContext(context.Background(), 10*time.Second),
+				),
+				&GetByJSON{},
+			).Into(&rsp)
+			require.Error(t, err)
+		}
+	})
+
+	t.Run("result pass", func(t *testing.T) {
+		req1, _ := http.NewRequest("GET", "https://ip.nf/me.json", nil)
+		res := cli.Do(context.Background(), req1)
+
+		req2, _ := http.NewRequest(http.MethodGet, "/", nil)
+		rw := mock.NewMockResponseWriter()
+		_ = httpx.ResponseFrom(res).WriteTo(rw, req2, nil)
+
+		require.Equal(t, 200, rw.StatusCode)
+	})
+}
