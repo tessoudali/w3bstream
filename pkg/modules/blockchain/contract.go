d72375e4f08a3be2cb2b65d3680d2f8cf9e3659a
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2023-06-13 10:39:23 +0800

    feat(blockchain): keep the dependencies within project and chainID (#657)

--- pkg/modules/blockchain/contract.go
+++ pkg/modules/blockchain/contract.go
@@ -1,16 +1,20 @@
 package blockchain
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"math/big"
-	"strings"
+	"sort"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 
+	"github.com/machinefi/w3bstream/pkg/depends/kit/sqlx"
 	"github.com/machinefi/w3bstream/pkg/depends/kit/sqlx/builder"
 	"github.com/machinefi/w3bstream/pkg/models"
 	"github.com/machinefi/w3bstream/pkg/types"
@@ -20,6 +24,12 @@ type contract struct {
 	*monitor
 	listInterval  time.Duration
 	blockInterval uint64
+}
+
+// a group of models.ContractLog, will list the chain and update current height togither
+type listChainGroup struct {
+	toBlock uint64
+	cs      []*models.ContractLog
 }
 
 func (t *contract) run(ctx context.Context) {
@@ -34,7 +44,6 @@ func (t *contract) run(ctx context.Context) {
 func (t *contract) do(ctx context.Context) {
 	d := types.MustMonitorDBExecutorFromContext(ctx)
 	l := types.MustLoggerFromContext(ctx)
-	ethcli := types.MustETHClientConfigFromContext(ctx)
 	m := &models.ContractLog{}
 
 	_, l = l.Start(ctx, "contract.run")
@@ -48,106 +57,271 @@ func (t *contract) do(ctx context.Context) {
 		l.Error(errors.Wrap(err, "list contractlog db failed"))
 		return
 	}
-	for _, c := range cs {
-		chainAddress, ok := ethcli.Clients[uint32(c.ChainID)]
-		if !ok {
-			l.WithValues("chainID", c.ChainID).Error(errors.New("blockchain not exist"))
-			continue
-		}
-		toBlock, err := t.listChainAndSendEvent(ctx, &c, chainAddress)
+
+	gs, err := t.getListChainGroups(ctx, cs)
+	if err != nil {
+		l.Error(errors.Wrap(err, "get lister units failed"))
+		return
+	}
+
+	for _, g := range gs {
+		toBlock, err := t.listChainAndSendEvent(ctx, g)
 		if err != nil {
-			l.Error(errors.Wrap(err, "list contractlog db failed"))
+			l.Error(errors.Wrap(err, "list chain and send event failed"))
 			continue
 		}
 
-		c.BlockCurrent = toBlock + 1
-		if c.BlockEnd > 0 && c.BlockCurrent >= c.BlockEnd {
-			c.Uniq = c.ContractLogID
-		}
-		if err := c.UpdateByID(d); err != nil {
+		if err := sqlx.NewTasks(d).With(
+			func(d sqlx.DBExecutor) error {
+				for _, c := range g.cs {
+					c.BlockCurrent = toBlock + 1
+					if c.BlockEnd > 0 && c.BlockCurrent >= c.BlockEnd {
+						c.Uniq = c.ContractLogID
+					}
+					if err := c.UpdateByID(d); err != nil {
+						return err
+					}
+				}
+				return nil
+			},
+		).Do(); err != nil {
 			l.Error(errors.Wrap(err, "update contractlog db failed"))
 		}
 	}
 }
 
-func (t *contract) listChainAndSendEvent(ctx context.Context, c *models.ContractLog, address string) (uint64, error) {
+func (t *contract) getListChainGroups(ctx context.Context, cs []models.ContractLog) ([]*listChainGroup, error) {
 	l := types.MustLoggerFromContext(ctx)
+
+	_, l = l.Start(ctx, "contract.getListChainGroups")
+	defer l.End()
+
+	us := t.groupContractLog(cs)
+	t.pruneListChainGroups(us)
+	if err := t.setToBlock(ctx, us); err != nil {
+		return nil, err
+	}
+	return us, nil
+}
+
+// projectName + chainID -> contractLog list
+func (t *contract) groupContractLog(cs []models.ContractLog) []*listChainGroup {
+	groups := make(map[string][]*models.ContractLog)
+
+	for i := range cs {
+		key := fmt.Sprintf("%s_%d", cs[i].ProjectName, cs[i].ChainID)
+		groups[key] = append(groups[key], &cs[i])
+	}
+
+	ret := []*listChainGroup{}
+	for _, cs := range groups {
+		ret = append(ret, &listChainGroup{
+			cs: cs,
+		})
+	}
+	return ret
+}
+
+func (t *contract) pruneListChainGroups(gs []*listChainGroup) {
+	for _, g := range gs {
+		sort.SliceStable(g.cs, func(i, j int) bool {
+			return g.cs[i].BlockCurrent < g.cs[j].BlockCurrent
+		})
+
+		if g.cs[0].BlockCurrent == g.cs[len(g.cs)-1].BlockCurrent {
+			continue
+		}
+		for i := range g.cs {
+			if i == 0 {
+				continue
+			}
+			if g.cs[i].BlockCurrent != g.cs[i-1].BlockCurrent {
+				g.toBlock = g.cs[i].BlockCurrent - 1
+				g.cs = g.cs[:i]
+				break
+			}
+		}
+	}
+}
+
+func (t *contract) setToBlock(ctx context.Context, gs []*listChainGroup) error {
+	l := types.MustLoggerFromContext(ctx)
+	ethcli := types.MustETHClientConfigFromContext(ctx)
+
+	_, l = l.Start(ctx, "contract.setToBlock")
+	defer l.End()
+
+	for _, g := range gs {
+		c := g.cs[0]
+
+		chainAddress, ok := ethcli.Clients[uint32(c.ChainID)]
+		if !ok {
+			err := errors.New("blockchain not exist")
+			l.WithValues("chainID", c.ChainID).Error(err)
+			return err
+		}
+
+		cli, err := ethclient.Dial(chainAddress)
+		if err != nil {
+			l.WithValues("chainID", c.ChainID).Error(errors.Wrap(err, "dial eth address failed"))
+			return err
+		}
+		currHeight, err := cli.BlockNumber(context.Background())
+		if err != nil {
+			l.Error(errors.Wrap(err, "get blockchain current height failed"))
+			return err
+		}
+
+		to := c.BlockCurrent + t.blockInterval
+		if to > currHeight {
+			to = currHeight
+		}
+		for _, c := range g.cs {
+			if c.BlockEnd > 0 && to > c.BlockEnd {
+				to = c.BlockEnd
+			}
+		}
+		if g.toBlock == 0 {
+			g.toBlock = to
+		}
+		if g.toBlock > to {
+			g.toBlock = to
+		}
+	}
+	return nil
+}
+
+func (t *contract) listChainAndSendEvent(ctx context.Context, g *listChainGroup) (uint64, error) {
+	l := types.MustLoggerFromContext(ctx)
+	ethcli := types.MustETHClientConfigFromContext(ctx)
 
 	_, l = l.Start(ctx, "contract.listChainAndSendEvent")
 	defer l.End()
 
-	l = l.WithValues("type", "contract_log", "contract_log_id", c.ContractLogID)
+	c := g.cs[0]
 
-	cli, err := ethclient.Dial(address)
-	if err != nil {
+	l = l.WithValues("chainID", c.ChainID, "projectName", c.ProjectName)
+
+	chainAddress, ok := ethcli.Clients[uint32(c.ChainID)]
+	if !ok {
+		err := errors.New("blockchain not exist")
 		l.Error(err)
 		return 0, err
 	}
 
-	from, to, err := t.getBlockRange(ctx, cli, c)
+	cli, err := ethclient.Dial(chainAddress)
 	if err != nil {
-		l.Error(err)
+		l.Error(errors.Wrap(err, "dial eth address failed"))
 		return 0, err
 	}
+
+	from, to := c.BlockCurrent, g.toBlock
+
 	if from > to {
 		l.WithValues("from block", from, "to block", to).Debug("no new block")
 		return to, nil
 	}
 	l.WithValues("from block", from, "to block", to).Debug("find new block")
 
+	as, mas := t.getAddresses(g.cs)
+	ts, mts := t.getTopic(g.cs)
 	query := ethereum.FilterQuery{
 		FromBlock: big.NewInt(int64(from)),
 		ToBlock:   big.NewInt(int64(to)),
-		Addresses: []common.Address{
-			common.HexToAddress(c.ContractAddress),
-		},
-		Topics: t.getTopic(c),
+		Addresses: as,
+		Topics:    ts,
 	}
 	logs, err := cli.FilterLogs(context.Background(), query)
 	if err != nil {
-		l.Error(err)
+		l.Error(errors.Wrap(err, "filter event logs failed"))
 		return 0, err
 	}
-	for _, log := range logs {
-		data, err := log.MarshalJSON()
+	for i := range logs {
+		cs := t.getExpectedContractLogs(&logs[i], mas, mts)
+		if len(cs) == 0 {
+			err := errors.New("cannot find expected contract log")
+			l.Error(err)
+			return 0, err
+		}
+
+		data, err := logs[i].MarshalJSON()
 		if err != nil {
 			return 0, err
 		}
-		if err := t.sendEvent(ctx, data, c.ProjectName, c.EventType); err != nil {
-			return 0, err
+		for _, c := range cs {
+			if err := t.sendEvent(ctx, data, c.ProjectName, c.EventType); err != nil {
+				return 0, err
+			}
 		}
 	}
 	return to, nil
 }
 
-func (t *contract) getBlockRange(ctx context.Context, cli *ethclient.Client, c *models.ContractLog) (uint64, uint64, error) {
-	l := types.MustLoggerFromContext(ctx)
+func (t *contract) getExpectedContractLogs(log *ethtypes.Log, mas map[*models.ContractLog]common.Address, mts map[*models.ContractLog][]*common.Hash) []*models.ContractLog {
+	res := []*models.ContractLog{}
 
-	_, l = l.Start(ctx, "contract.getBlockRange")
-	defer l.End()
+	for c, addr := range mas {
+		if bytes.Equal(addr.Bytes(), log.Address.Bytes()) {
+			ts := mts[c]
 
-	currHeight, err := cli.BlockNumber(context.Background())
-	if err != nil {
-		l.Error(err)
-		return 0, 0, err
+			for i, contractLogTopic := range ts {
+				if contractLogTopic == nil {
+					continue
+				}
+				if len(log.Topics) > i && bytes.Equal(log.Topics[i].Bytes(), contractLogTopic.Bytes()) {
+					continue
+				}
+				goto Next
+			}
+			res = append(res, c)
+		}
+	Next:
 	}
-	from := c.BlockCurrent
-	to := c.BlockCurrent + t.blockInterval
-	if to > currHeight {
-		to = currHeight
-	}
-	if c.BlockEnd > 0 && to > c.BlockEnd {
-		to = c.BlockEnd
-	}
-	return from, to, nil
+	return res
 }
 
-func (t *contract) getTopic(c *models.ContractLog) [][]common.Hash {
+func (t *contract) getAddresses(cs []*models.ContractLog) ([]common.Address, map[*models.ContractLog]common.Address) {
+	as := []common.Address{}
+	mas := make(map[*models.ContractLog]common.Address)
+	for _, c := range cs {
+		a := common.HexToAddress(c.ContractAddress)
+		as = append(as, a)
+		mas[c] = a
+	}
+	return as, mas
+}
+
+func (t *contract) getTopic(cs []*models.ContractLog) ([][]common.Hash, map[*models.ContractLog][]*common.Hash) {
 	res := make([][]common.Hash, 4)
-	res[0] = t.parseTopic(c.Topic0)
-	res[1] = t.parseTopic(c.Topic1)
-	res[2] = t.parseTopic(c.Topic2)
-	res[3] = t.parseTopic(c.Topic3)
+	mres := make(map[*models.ContractLog][]*common.Hash)
+
+	for _, c := range cs {
+		h0 := t.parseTopic(c.Topic0)
+		mres[c] = append(mres[c], h0)
+		if h0 != nil {
+			res[0] = append(res[0], *h0)
+		}
+
+		h1 := t.parseTopic(c.Topic1)
+		mres[c] = append(mres[c], h1)
+		if h1 != nil {
+			res[1] = append(res[1], *h1)
+		}
+
+		h2 := t.parseTopic(c.Topic2)
+		mres[c] = append(mres[c], h2)
+		if h2 != nil {
+			res[2] = append(res[2], *h2)
+		}
+
+		h3 := t.parseTopic(c.Topic3)
+		mres[c] = append(mres[c], h3)
+		if h3 != nil {
+			res[3] = append(res[3], *h3)
+		}
+
+	}
+
 	if len(res[3]) == 0 {
 		res = res[:3]
 		if len(res[2]) == 0 {
@@ -160,18 +334,13 @@ func (t *contract) getTopic(c *models.ContractLog) [][]common.Hash {
 			}
 		}
 	}
-	return res
+	return res, mres
 }
 
-func (t *contract) parseTopic(ts string) []common.Hash {
-	res := make([]common.Hash, 0)
-	if ts == "" {
-		return res
+func (t *contract) parseTopic(tStr string) *common.Hash {
+	if tStr == "" {
+		return nil
 	}
-	ss := strings.Split(ts, ",")
-	for _, s := range ss {
-		h := common.HexToHash(s)
-		res = append(res, h)
-	}
-	return res
+	h := common.HexToHash(tStr)
+	return &h
 }
