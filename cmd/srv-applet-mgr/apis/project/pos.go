f0c114809bc87ecef306f60e46885a8b3a770f22
hunshenshi <289517357@qq.com>
2023-06-16 17:04:03 +0800

    chore(project): make sure monitor and metrics deleted before project creation (#664)

--- cmd/srv-applet-mgr/apis/project/pos.go
+++ cmd/srv-applet-mgr/apis/project/pos.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/machinefi/w3bstream/cmd/srv-applet-mgr/apis/middleware"
 	"github.com/machinefi/w3bstream/pkg/depends/kit/httptransport/httpx"
+	"github.com/machinefi/w3bstream/pkg/modules/blockchain"
+	"github.com/machinefi/w3bstream/pkg/modules/metrics"
 	"github.com/machinefi/w3bstream/pkg/modules/project"
 )
 
@@ -14,13 +16,18 @@ type CreateProject struct {
 }
 
 func (r *CreateProject) Output(ctx context.Context) (interface{}, error) {
-	ctx = middleware.MustCurrentAccountFromContext(ctx).WithAccount(ctx)
+	acc := middleware.MustCurrentAccountFromContext(ctx)
+	ctx = acc.WithAccount(ctx)
 
 	prefix, err := middleware.ProjectNameModifier(ctx)
 	if err != nil {
 		return nil, err
 	}
 	r.Name = prefix + r.Name
+
+	// make sure monitor and metrics deleted
+	blockchain.RemoveMonitor(ctx, r.Name)
+	metrics.RemoveMetrics(ctx, acc.AccountID.String(), r.Name)
 
 	rsp, err := project.Create(ctx, &r.CreateReq)
 	if err != nil {
