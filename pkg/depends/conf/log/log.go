b5f970d5392e5c58393f8d3c4391a62178127341
hunshenshi <289517357@qq.com>
2023-07-18 19:02:24 +0800

    feat(log): export log to clickhouse (#695)
    * feat(log): export log to clickhouse
    
    * feat(log): update comment

--- pkg/depends/conf/log/log.go
+++ pkg/depends/conf/log/log.go
@@ -2,9 +2,13 @@ package log
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"os"
 	"runtime"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel/sdk/trace"
@@ -18,6 +22,7 @@ type Log struct {
 	Level        Level            `env:""`
 	Output       LoggerOutputType `env:""`
 	Format       LoggerFormatType
+	CKEndpoint   string             `env:""`
 	Exporter     trace.SpanExporter `env:"-"`
 	ReportCaller bool
 }
@@ -52,6 +57,13 @@ func (l *Log) InitLogrus() {
 	// TODO add hook with goid meta logrus.AddHook(goid.Default)
 	logrus.AddHook(&ProjectAndMetaHook{l.Name})
 
+	if l.CKEndpoint != "" {
+		if ckHook, err := newClickhouseHook(l.CKEndpoint); err != nil {
+			logrus.Errorf("new ck hook error: %s", err)
+		} else {
+			logrus.AddHook(ckHook)
+		}
+	}
 	logrus.SetOutput(os.Stdout)
 }
 
@@ -67,6 +79,114 @@ func (l *Log) InitSpanLog() {
 func (l *Log) Init() {
 	l.InitLogrus()
 	l.InitSpanLog()
+}
+
+var ckBatchCount = 1000
+
+type ClickhouseHook struct {
+	CKEndpoint     string
+	ckDB           *sql.DB
+	insertSql      string
+	entries        []logrus.Entry
+	lastInsertTime time.Time
+	duration       time.Duration
+	lock           *sync.Mutex
+}
+
+func newClickhouseHook(endpoint string) (*ClickhouseHook, error) {
+	clickhouseHook := &ClickhouseHook{CKEndpoint: endpoint, entries: make([]logrus.Entry, 0, ckBatchCount*2)}
+	clickhouseHook.insertSql = `INSERT INTO w3b.server_logs (Timestamp, ProjectName, Msg) VALUES (?, ?, ?)`
+	clickhouseHook.lastInsertTime = time.Now()
+	clickhouseHook.duration = time.Second
+	clickhouseHook.lock = &sync.Mutex{}
+	db, err := clickhouseHook.connection()
+	if err != nil {
+		return nil, err
+	}
+	clickhouseHook.ckDB = db
+	return clickhouseHook, nil
+}
+
+func (ck *ClickhouseHook) Fire(entry *logrus.Entry) error {
+	if !ck.ping() {
+		conn, err := ck.connection()
+		if err != nil {
+			logrus.Errorf("ck connection error: %s", err)
+			return err
+		}
+		ck.ckDB = conn
+	}
+
+	ck.lock.Lock()
+	defer ck.lock.Unlock()
+	ck.entries = append(ck.entries, *entry)
+	if len(ck.entries) >= ckBatchCount || time.Since(ck.lastInsertTime) > ck.duration {
+		ck.insertLogs(ck.entries)
+	}
+	return nil
+}
+
+func (ck *ClickhouseHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (ck *ClickhouseHook) connection() (*sql.DB, error) {
+	conn, err := sql.Open("clickhouse", ck.CKEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	err = conn.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func (ck *ClickhouseHook) ping() bool {
+	if err := ck.ckDB.Ping(); err != nil {
+		logrus.Errorf("ck ping error: %s", err)
+		return false
+	}
+	return true
+}
+
+func (ck *ClickhouseHook) insertLogs(entries []logrus.Entry) error {
+	err := ck.doWithTx(func(tx *sql.Tx) error {
+		statement, err := tx.PrepareContext(context.Background(), ck.insertSql)
+		if err != nil {
+			return fmt.Errorf(" ck prepareContext error: %w", err)
+		}
+		defer func() {
+			if err := statement.Close(); err != nil {
+				logrus.Errorf("ck statement close error: %s", err)
+			}
+		}()
+		for _, item := range entries {
+			msg, _ := item.Bytes()
+			if _, err := statement.ExecContext(context.Background(), item.Time, item.Data["@prj"], msg); err != nil {
+				return fmt.Errorf("ck execContext error:%w", err)
+			}
+		}
+		return nil
+	})
+	if err == nil {
+		ck.entries = ck.entries[:0]
+	}
+	return err
+}
+
+func (ck *ClickhouseHook) doWithTx(fn func(tx *sql.Tx) error) error {
+	tx, err := ck.ckDB.Begin()
+	if err != nil {
+		return fmt.Errorf("ck tx begin error: %w", err)
+	}
+	defer func() {
+		tx.Rollback()
+	}()
+	if err := fn(tx); err != nil {
+		return err
+	}
+	return tx.Commit()
 }
 
 type ProjectAndMetaHook struct {
